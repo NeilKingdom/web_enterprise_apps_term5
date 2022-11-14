e7eff7071b2fe36b069699dc4073a3df0ee5116c
NeilKingdom <neil@neilkingdom.xyz>
2022-11-13 21:52:16 -0500

    Finished lab4

---
+++ Lab4/mavenproject4/src/main/java/com/mycompany/mavenproject4/AppUser.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject4;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.security.enterprise.identitystore.PasswordHash;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author neil
+ */
+@Entity
+@Table(name = "APPUSER")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "AppUser.findAll", query = "SELECT c FROM AppUser c"),
+    @NamedQuery(name = "AppUser.findById", query = "SELECT c FROM AppUser c WHERE c.id = :id"),
+    @NamedQuery(name = "AppUser.findByUserid", query = "SELECT c FROM AppUser c WHERE c.userid = :userid"),
+    @NamedQuery(name = "AppUser.findByPassword", query = "SELECT c FROM AppUser c WHERE c.password = :password"),
+    @NamedQuery(name = "AppUser.findByGroupname", query = "SELECT c FROM AppUser c WHERE c.groupname = :groupname")})
+public class AppUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Size(max = 255)
+    @Column(name = "USERID")
+    private String userid;
+    @Size(max = 255)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Size(max = 255)
+    @Column(name = "GROUPNAME")
+    private String groupname;
+
+    public AppUser() {
+    }
+
+    public AppUser(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return "";
+    }
+
+    public void setPassword(String password) {
+        // initialize a PasswordHash object which will generate password hashes
+        Instance<? extends PasswordHash> instance = CDI.current().select(Pbkdf2PasswordHash.class);
+        PasswordHash passwordHash = instance.get();
+        passwordHash.initialize(new HashMap<String,String>()); // todo: are the defaults good enough?
+        // now we can generate a password entry for a given password
+        String passwordEntry = "mySecretPassword"; //pretend the user has chosen a password mySecretPassword
+        passwordEntry = passwordHash.generate(passwordEntry.toCharArray());
+        //at this point, passwordEntry refers to a salted/hashed password entry corresponding to mySecretPassword
+        
+        this.password = passwordEntry;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AppUser)) {
+            return false;
+        }
+        AppUser other = (AppUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.mavenproject4.AppUser[ id=" + id + " ]";
+    }
+    
+}
